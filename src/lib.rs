d67cc9920aa9b4883393e411fb4fa2ddd4c498b5
LGUG2Z <LGUG2Z@fastmail.com>
2024-09-17 17:14:07 -0700

    refactor(safety): remove all remains unwraps and runtime panics

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,7 @@ use base64::Engine as _;
 use image::RgbaImage;
 use std::ffi::OsStr;
 use std::ffi::OsString;
-use std::mem::{self, MaybeUninit};
+use std::mem::MaybeUninit;
 use std::os::windows::ffi::OsStrExt;
 use std::os::windows::ffi::OsStringExt;
 use std::ptr;
@@ -17,7 +17,7 @@ use windows::Win32::{
     Foundation::HWND,
     Graphics::Gdi::{
         DeleteObject, GetDC, GetDIBits, ReleaseDC, BITMAP, BITMAPINFO, BITMAPINFOHEADER, BI_RGB,
-        DIB_RGB_COLORS, HDC, HGDIOBJ,
+        DIB_RGB_COLORS, HGDIOBJ,
     },
     Storage::FileSystem::FILE_FLAGS_AND_ATTRIBUTES,
     UI::WindowsAndMessaging::{GetIconInfo, HICON},
@@ -42,33 +42,32 @@ unsafe fn get_hicon(file_path: &str) -> Option<HICON> {
     }
 }
 
-unsafe fn icon_to_image(icon: HICON) -> RgbaImage {
-    let bitmap_size_i32 = i32::try_from(mem::size_of::<BITMAP>()).unwrap();
-    let biheader_size_u32 = u32::try_from(mem::size_of::<BITMAPINFOHEADER>()).unwrap();
+unsafe fn icon_to_image(icon: HICON) -> Result<RgbaImage, Box<dyn std::error::Error>> {
+    let bitmap_size_i32 = std::mem::size_of::<BITMAP>() as i32;
+    let biheader_size_u32 = std::mem::size_of::<BITMAPINFOHEADER>() as u32;
 
     let mut info = MaybeUninit::uninit();
-    GetIconInfo(icon, info.as_mut_ptr()).unwrap();
+    GetIconInfo(icon, info.as_mut_ptr())?;
     let info = info.assume_init();
-    DeleteObject(info.hbmMask).unwrap();
+    let _ = DeleteObject(info.hbmMask);
 
     let mut bitmap: MaybeUninit<BITMAP> = MaybeUninit::uninit();
-    let result = GetObjectW(
+    let _ = GetObjectW(
         HGDIOBJ(info.hbmColor.0),
         bitmap_size_i32,
         Some(bitmap.as_mut_ptr().cast()),
     );
-    assert!(result == bitmap_size_i32);
+
     let bitmap = bitmap.assume_init();
 
-    let width_u32 = u32::try_from(bitmap.bmWidth).unwrap();
-    let height_u32 = u32::try_from(bitmap.bmHeight).unwrap();
-    let width_usize = usize::try_from(bitmap.bmWidth).unwrap();
-    let height_usize = usize::try_from(bitmap.bmHeight).unwrap();
-    let buf_size = width_usize.checked_mul(height_usize).unwrap();
+    let width_u32 = bitmap.bmWidth as u32;
+    let height_u32 = bitmap.bmHeight as u32;
+    let width_usize = bitmap.bmWidth as usize;
+    let height_usize = bitmap.bmHeight as usize;
+    let buf_size = width_usize.checked_mul(height_usize).unwrap_or_default();
     let mut buf: Vec<u32> = Vec::with_capacity(buf_size);
 
     let dc = GetDC(HWND(ptr::null_mut()));
-    assert!(dc != HDC(ptr::null_mut()));
 
     let mut bitmap_info = BITMAPINFO {
         bmiHeader: BITMAPINFOHEADER {
@@ -86,7 +85,7 @@ unsafe fn icon_to_image(icon: HICON) -> RgbaImage {
         },
         bmiColors: [Default::default()],
     };
-    let result = GetDIBits(
+    let _ = GetDIBits(
         dc,
         info.hbmColor,
         0,
@@ -95,20 +94,18 @@ unsafe fn icon_to_image(icon: HICON) -> RgbaImage {
         &mut bitmap_info,
         DIB_RGB_COLORS,
     );
-    assert!(result == bitmap.bmHeight);
     buf.set_len(buf.capacity());
 
-    let result = ReleaseDC(HWND(ptr::null_mut()), dc);
-    assert!(result == 1);
-    DeleteObject(info.hbmColor).unwrap();
+    let _ = ReleaseDC(HWND(ptr::null_mut()), dc);
+    let _ = DeleteObject(info.hbmColor);
 
-    RgbaImage::from_fn(width_u32, height_u32, |x, y| {
-        let x_usize = usize::try_from(x).unwrap();
-        let y_usize = usize::try_from(y).unwrap();
+    Ok(RgbaImage::from_fn(width_u32, height_u32, |x, y| {
+        let x_usize = x as usize;
+        let y_usize = y as usize;
         let idx = y_usize * width_usize + x_usize;
         let [b, g, r, a] = buf[idx].to_le_bytes();
         [r, g, b, a].into()
-    })
+    }))
 }
 
 pub fn get_process_path(process_id: u32) -> Result<String, windows::core::Error> {
@@ -120,7 +117,7 @@ pub fn get_process_path(process_id: u32) -> Result<String, windows::core::Error>
         )?;
         let mut buffer = vec![0u16; 1024];
         let size = K32GetModuleFileNameExW(HANDLE(process_handle.0), None, &mut buffer);
-        CloseHandle(process_handle).unwrap();
+        CloseHandle(process_handle)?;
 
         if size == 0 {
             return Err(windows::core::Error::from_win32());
@@ -148,7 +145,7 @@ pub fn get_icon_by_process_id(process_id: u32) -> Option<RgbaImage> {
 
 pub fn get_icon_by_path(path: &str) -> Option<RgbaImage> {
     unsafe {
-        get_hicon(path).map(|icon| icon_to_image(icon))
+        get_hicon(path).map(|icon| icon_to_image(icon).ok())?
     }
 }
 
@@ -167,8 +164,7 @@ pub fn get_icon_base64_by_path(path: &str) -> Option<String> {
             .write_to(
                 &mut std::io::Cursor::new(&mut buffer),
                 image::ImageFormat::Png,
-            )
-            .unwrap();
+            ).ok();
         Some(general_purpose::STANDARD.encode(buffer))
     } else {
         None
