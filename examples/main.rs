e14c58acd859c2e9fc4f22e40b209d9e0b5d3a14
davor-skontra <thekrad23@gmail.com>
2025-01-10 01:14:42 +0200

    Unwrap calls in main
    This is an example intended for the original repo so no need sprucing
    this up. Would consider nuking the whoöe examples folder were it up to
    me.

--- examples/main.rs
+++ examples/main.rs
@@ -7,29 +7,29 @@ fn main() {
     let _ = std::fs::create_dir("output");
 
     let icon = get_icon_by_path("C:\\Windows\\System32\\notepad.exe");
-    icon.save("output/notepad.png").unwrap();
+    icon.unwrap().save("output/notepad.png").unwrap();
 
     let icon = get_icon_by_path("C:\\Windows\\System32\\calc.exe");
-    icon.save("output/calc.png").unwrap();
+    icon.unwrap().save("output/calc.png").unwrap();
 
     let icon = get_icon_by_path("C:\\Windows\\System32\\cmd.exe");
-    icon.save("output/cmd.png").unwrap();
+    icon.unwrap().save("output/cmd.png").unwrap();
 
-    let base64 = get_icon_base64_by_path("C:\\Windows\\System32\\notepad.exe");
-    println!("Notepad: {}", base64);
+    let base64 = get_icon_base64_by_path("C:\\Windows\\System32\\notepad.exe").unwrap();
+    println!("Notepad: {:?}", base64);
 
     let base64 = get_icon_base64_by_path("C:\\Windows\\System32\\calc.exe");
-    println!("Calc: {}", base64);
+    println!("Calc: {:?}", base64);
 
     let base64 = get_icon_base64_by_path("C:\\Windows\\System32\\cmd.exe");
-    println!("Cmd: {}", base64);
+    println!("Cmd: {:?}", base64);
 
     // Substitute the process id to test
     let process_id = 0000;
 
     let base64 = get_icon_base64_by_process_id(process_id);
-    println!("Process {}: {}", process_id, base64);
+    println!("Process {}: {:?}", process_id, base64);
 
     let icon = get_icon_by_process_id(process_id);
-    icon.save("output/process.png").unwrap();
+    icon.unwrap().save("output/process.png").unwrap();
 }
